abdf13e7dc9e0a27b13292c1f664e9206215c1ab
James White <james@thewhites.co>
2024-01-13 11:46:03 -0600

    mixin for list-like classes

--- src/mixins/collection.js
+++ src/mixins/collection.js
@@ -1,56 +1,97 @@
+import * as diff from 'fast-array-diff';
 import { mixin } from './mixin';
 import { message_passing } from './message_passing';
+import { default_compare } from './sortable';
+
+const compare = ({ key: a }, { key: b }) => a === b;
+
+const make_patch(collection) {
+  const patch = [];
+  let offset = 0, type, l1, l2;
+  return {
+    append(oldPos, items) {
+      const newPos = oldPos + offset;
+      patch.push({ type: 'add', oldPos, newPos, items });
+      collection.push(...items);
+    },
+    insert(oldPos, item) {
+      if (type === 'add') {
+        l1.items.push(item);
+      } else {
+        const newPos = oldPos + offset++;
+        [l2, l1] = [l1, { type: type = 'add', oldPos, newPos, items: [item] }];
+        patch.push(l1);
+      }
+      collection.splice(oldPos, 0, item);
+    },
+    replace(oldPos, item) {
+      if (type === 'replace') {
+        l2.length++;
+        l1.items.push(item);
+      } else {
+        type = 'replace';
+        const newPos = oldPos + offset;
+        patch.push(l2 = { type: 'remove', oldPos, newPos, length: 1 });
+        patch.push(l1 = { type: 'add', oldPos, newPos, items: [item] });
+      }
+      collection[index] = item;
+    },
+    result() {
+      return patch;
+    }
+  };
+}
 
 export const collection = mixin(function (proto, cls) {
-  message_passing(proto, cls);
+  const { pub, sub } = message_passing(proto, cls);
 
   sub('custom-element', 'connect', function () { this._collection = [] });
   
-  cls.prototype.get_collection = function () { return this._collection };
-  cls.prototype.set_collection = function (items) {
+  proto.get_collection = function () { return this._collection };
+  proto.set_collection = function (items) {
     this._collection = items || [];
     if (this._compare) this.sort();
     pub('collection', 'set', { items });
     return this;
   };
 
-  // for non-sortable collections
-  cls.prototype.collection_append = function(items) {
-    this._collection.push(...items);
-    pub('collection', 'append', { items });
-    return this;
-  };
-  cls.prototype.collection_insert = function(index, items) {
-    if (items === undefined) [index, items] = [0, index];
-    this._collection.splice(index, 0, ...items);
-    pub('collection', 'insert', { index, items });
-    return this;
-  };
-
-  // for sortable collections
+  // add/update for sortable collections
   function update_collection(self, items, replace) {
     items = self.sort(items);
+    const patch = make_patch(self._collection);
     const len = self._collection.length;
     const blen = items.length;
     for (var a = 0, b = 0; a < alen || b < blen; a++)  {
       const itemb = items[b];
       const result = self._compare(self._collection[a], itemb);
       if (result >= 0) {
-        if (result > 0) self._collection.splice(a, 0, itemb);
-        else if (replace) self._collection[a] = itemb;
+        if (result > 0) patch.insert(a, itemb);
+        else if (replace) patch.replace(a, itemb);
         b++;
       }
     }
-    if (b < blen) self._collection.push(...items);
+    if (b < blen) patch.append(items);
+    return patch.result();
   }
-  cls.prototype.collection_add = function (items) {
-    update_collection(this, items);
-    pub('collection', 'add', { items, indexes });
+
+  proto.collection_add = function (items) {
+    const len = this._collection.length;
+    let patch = [{ type: 'add', oldPos: len, newPos: len, items }];
+    if (this._compare) patch = update_collection(this, items);
+    else this._collection.push(...items);
+    pub('collection', 'update', { patch });
     return this;
   };
-  cls.prototype.collection_update = function (items) {
-    update_collection(this, items, true);
-    pub('collection', 'update', { items, indexes });
+
+  proto.collection_update = function (items) {
+    let patch;
+    if (this._compare) {
+      patch = update_collection(this, items, true);
+    } else {
+      patch = diff.getPatch(this._collection, items, default_compare)
+      this._collection = diff.applyPatch(this._collection, patch);
+    }
+    pub('collection', 'update', { patch });
     return this;
   };
 
